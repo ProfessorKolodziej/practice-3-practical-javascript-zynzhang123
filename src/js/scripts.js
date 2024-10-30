68b54bc2cd5204004c85ce1f700a4ad52f857822
Yineng Zhang <zynzhang@bu.edu>
2024-10-30 18:19:17 -0400

    practice10.30

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -5,3 +5,37 @@
 // - Do not use onclick - use addEventListener instead
 // - Run npm run test regularly to check autograding
 // - You'll need to link this file to your HTML :)
+
+console.log('Hello!');
+
+const lala = 'Nancy';
+console.log(lala);
+console.log(typeof lala);
+
+const b = 4 < 3;
+console.log(b);
+
+let age = 20;
+age += 1;
+console.log(age);
+
+//Practice for JS: show and hide the paragraph
+
+//find the element to hide
+const toggledp = document.querySelector('.show');
+console.log(toggledp);
+
+//write a function to show/hide the paragraph
+function toggledpf() {
+    toggledp.classList.toggle('show');
+    toggledp.classList.toggle('hide');
+    console.log("Working!");
+    console.log(toggledp);
+}
+
+
+
+//find the button and make action
+const button = document.querySelector('#toggle-control');
+console.log(button);
+button.addEventListener('click', toggledpf);
